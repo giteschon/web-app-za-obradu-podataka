77fc206c2d97cebbcd5670c9c123eb31fb03e5f7
giteschon <ivana.kasalo93@gmail.com>
2022-09-22 08:51:18 +0200

    INICIJALNO: postavljanje Spring projekta

---
+++ src/test/java/hr/ivanakasalo/zadatak/ZadatakApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.ivanakasalo.zadatak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadatakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
