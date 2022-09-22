b33a971714a8941b575d5ff6ea78c8989024954b
giteschon <ivana.kasalo93@gmail.com>
2022-09-22 10:34:18 +0200

    DODANO: Postavljanje dev i prod profila za spajanje na bazu. Dodana model klasa Osoba.

---
+++ src/main/java/hr/ivanakasalo/zadatak/models/Osoba.java
@@ -0,0 +1,44 @@
+package hr.ivanakasalo.zadatak.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+/**
+ * Entitet Osoba
+ */
+@Entity
+public class Osoba {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String ime;
+    private String prezime;
+    private LocalDate datumRodjenja;
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public LocalDate getDatumRodjenja() {
+        return datumRodjenja;
+    }
+
+    public void setDatumRodjenja(LocalDate datumRodjenja) {
+        this.datumRodjenja = datumRodjenja;
+    }
+}
