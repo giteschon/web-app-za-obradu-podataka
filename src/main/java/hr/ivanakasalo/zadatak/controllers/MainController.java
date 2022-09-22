124bdeee9076b6ed948d7f85273d5043c88f841b
giteschon <ivana.kasalo93@gmail.com>
2022-09-22 18:05:45 +0200

    DODANO: Generiranje osoba Stranica za preuzimanje .csv datoteke

--- src/main/java/hr/ivanakasalo/zadatak/controllers/MainController.java
+++ src/main/java/hr/ivanakasalo/zadatak/controllers/MainController.java
@@ -1,9 +1,20 @@
 package hr.ivanakasalo.zadatak.controllers;
 
+import hr.ivanakasalo.zadatak.podaci.generiranje.GeneriranjeOsoba;
 import hr.ivanakasalo.zadatak.services.OsobaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.ByteArrayOutputStream;
 
 @Controller
 public class MainController {
@@ -11,14 +22,48 @@ public class MainController {
     @Autowired
     private OsobaService service;
 
+    private GeneriranjeOsoba generiranjeDatoteke;
+
+    public MainController() {
+        generiranjeDatoteke = new GeneriranjeOsoba();
+    }
+
     @GetMapping("")
     public String home() {
-        return "ucitavanje";
+        return "redirect:/ucitavanje";
     }
 
     @GetMapping("ucitavanje")
-    public String ucitavanjeDatoteke() {
-        service.spremi();
-        return "view/ucitavanje-view.html";
+    public ModelAndView ucitavanjeDatoteke() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("osobe", service.dohvatiSve());
+        modelAndView.setViewName("view/ucitavanje-view.html");
+
+        return modelAndView;
+    }
+
+    @PostMapping("ucitavanje")
+    public String ucitajDatoteku(@RequestParam MultipartFile file) {
+        service.spremiOsobe(file);
+        return "redirect:/ucitavanje";
+    }
+
+    @GetMapping("generiranje")
+    public String generiranjeCsvDatoteke() {
+
+        return "view/generiranje-view.html";
+    }
+
+
+    @PostMapping("generiranje")
+    @ResponseBody
+    public ResponseEntity<byte[]> getInvoice() {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"podaci.csv\"");
+        headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.CONTENT_DISPOSITION);
+        ByteArrayOutputStream baos = generiranjeDatoteke.generiraj(100);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .headers(headers)
+                .body(baos.toByteArray());
     }
 }
---
+++ src/main/java/hr/ivanakasalo/zadatak/podaci/generiranje/GeneriranjeOsoba.java
@@ -0,0 +1,105 @@
+package hr.ivanakasalo.zadatak.podaci.generiranje;
+
+import hr.ivanakasalo.zadatak.models.Osoba;
+import hr.ivanakasalo.zadatak.podaci.mapiranje.CsvMapiranje;
+import hr.ivanakasalo.zadatak.podaci.mapiranje.Podatak;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
+import java.time.Month;
+import java.util.*;
+
+public class GeneriranjeOsoba {
+
+    private static final String[] imena = {"Katarina",
+            "Iskra", "Maša", "Ivan", "Nikola", "Matej", "Isak", "Matija", "Marta",
+            "Jelena", "Lea", "Nina", "Milan", "Maksim", "Vanja", "Darija", "Aleksej", "Anastasija", "Magdalena",
+            "Aleksandar", "Tea", "Milica", "Marijana", "Klara", "Jan", "Andrija", "Borna", "Dora", "Arian", "Damjan"};
+
+    private static final String[] prezimena = {"Jukić",
+            "Topić", "Bašić", "Jurić", "Delić", "Bilić", "Šimić", "Muslim", "Grubišić", "Filipović", "Murtić",
+            "Zelenbaba", "Ujdur", "Grgurić", "Bagarić", "Jurčević", "Vergić", "Ibrahimbegović", "Kvočić", "Ljubas"};
+
+    private final Random random;
+
+    public GeneriranjeOsoba() {
+        this.random = new Random();
+    }
+
+    public ByteArrayOutputStream generiraj(int brojRedaka) {
+        StringBuilder sb = new StringBuilder();
+        List<String> nazivi = dodajNazive(sb);
+
+        int index = 0;
+        while (index < brojRedaka) {
+            dodajOsobu(sb, nazivi);
+            index++;
+        }
+
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            baos.write(sb.toString().getBytes(StandardCharsets.UTF_8));
+            return baos;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private List<String> dodajNazive(StringBuilder sb) {
+        List<String> nazivi = new ArrayList<>();
+        Class<?> clazz = Osoba.class;
+        Field[] declaredFields = clazz.getDeclaredFields();
+        Arrays.stream(declaredFields).forEach(f -> {
+            if (f.isAnnotationPresent(Podatak.class)) {
+                nazivi.add(f.getDeclaredAnnotation(Podatak.class).naziv());
+            }
+        });
+
+        Collections.shuffle(nazivi);
+
+        for (String naziv : nazivi) {
+            sb.append(naziv).append(CsvMapiranje.DELIMITER);
+        }
+        sb.append("\n");
+
+        return nazivi;
+    }
+
+    private void dodajOsobu(StringBuilder sb, List<String> nazivi) {
+        for (String n : nazivi) {
+            switch (n) {
+                case "Ime":
+                    sb.append(generirajIme(imena));
+                    break;
+                case "Prezime":
+                    sb.append(generirajIme(prezimena));
+                    break;
+                case "DatumRodjenja":
+                    sb.append(generirajDatum());
+            }
+            sb.append(CsvMapiranje.DELIMITER);
+        }
+        sb.append("\n");
+    }
+
+    /**
+     * Generiranje datuma u ISO formatu (yyyy-MM-dd)
+     *
+     * @return
+     */
+    private String generirajDatum() {
+        int godina = random.nextInt(1960, 2022);
+        int mjesec = random.nextInt(1, 12);
+        Month m = Month.of(mjesec);
+        int dan = random.nextInt(1, m.maxLength());
+
+        return String.format("%d-%02d-%02d", godina, mjesec, dan);
+    }
+
+    private String generirajIme(String[] imena) {
+        return imena[random.nextInt(imena.length)];
+    }
+}
